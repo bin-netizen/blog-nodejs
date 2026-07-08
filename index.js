e8da2e400aaa413e28d1b7025ccf973aa39e3094
Tran Gia Bao <zaaabao10112005@gmail.com>
2026-07-08 08:54:30 +0700

    add full project

---
+++ index.js
@@ -0,0 +1,15 @@
+const express = require('express');
+const morgan = require('morgan');
+
+const app = express();
+const port = 3000;
+
+app.use(morgan('combined'));
+
+app.get('/', (req, res) => {
+  res.send('Chào mừng đến với Blog cá nhân của tôi!');
+});
+
+app.listen(port, () => {
+  console.log(`Server đang chạy tại http://localhost:${port}`);
+});
