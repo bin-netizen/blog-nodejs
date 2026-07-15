e5f7e75714e470c222cb29d29e3361fa3b94c020
Tran Gia Bao <zaaabao10112005@gmail.com>
2026-07-15 12:26:48 +0700

    update: fix css + img path

--- index.js
+++ index.js
@@ -3,12 +3,20 @@ const morgan = require('morgan');
 
 const app = express();
 const port = 3000;
+const { engine } = require('express-handlebars');
+
+// Cấp quyền truy cập cho thư mục public
+app.use(express.static(path.join(__dirname, 'public')));
+// Cấu hình Handlebars
+app.engine('hbs', engine({ extname: '.hbs' }));
+app.set('view engine', 'hbs');
+app.set('views', './views'); // Chỉ định thư mục chứa giao diện
 
 app.use(morgan('combined'));
 
-app.get('/', (req, res) => {
-  res.send('Chào mừng đến với Blog cá nhân của tôi!');
-});
+app.get('/', function (req, res) {
+    res.render('home');
+  });
 
 app.listen(port, () => {
   console.log(`Server đang chạy tại http://localhost:${port}`);
