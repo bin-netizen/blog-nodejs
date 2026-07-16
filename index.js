1907bde03edc7a20170b981f69434589235b5efe
Tran Gia Bao <zaaabao10112005@gmail.com>
2026-07-16 12:35:19 +0700

    update search feature (q + type)

--- index.js
+++ index.js
@@ -1,5 +1,6 @@
 const express = require('express');
 const morgan = require('morgan');
+const path = require('path');
 
 const app = express();
 const port = 3000;
@@ -17,6 +18,23 @@ app.use(morgan('combined'));
 app.get('/', function (req, res) {
     res.render('home');
   });
+  // Route xử lý cho trang About
+app.get('/about', (req, res) => {
+    res.render('about');
+});
+
+// Route xử lý cho trang Liên hệ
+app.get('/contact', (req, res) => {
+    res.render('contact');
+});
+
+// Route hiển thị trang Tìm kiếm
+app.get('/search', (req, res) => {
+    // req.query chứa toàn bộ các parameters trên URL
+    console.log("Từ khóa tìm kiếm:", req.query.q);
+
+    res.render('search');
+});
 
 app.listen(port, () => {
   console.log(`Server đang chạy tại http://localhost:${port}`);
